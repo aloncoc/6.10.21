9c1af73a158bc1d675dcc9ca1da395f75f41b596
aloncoc <89935432+aloncoc@users.noreply.github.com>
2021-10-10 13:54:41 +0300

    Create 6.10 homework (2).py

---
+++ 6.10 homework (2).py
@@ -0,0 +1,10 @@
+while True:
+    a = int(input('enter number:'))
+    b = int(input('enter number:'))
+    if  a ==0 or b ==0:
+        continue
+    if b>a:
+        print('bigger')
+    else:
+            break
+
